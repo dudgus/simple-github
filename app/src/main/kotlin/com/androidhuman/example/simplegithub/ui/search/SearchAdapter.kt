6f149ba30bb38b66e7c563a3a1d9b3d60c8f91d9
Younghyun Kim <yhyun.kim@gmail.com>
2018-11-04 20:41:23 +0900

    kotlinx.android.synthetic 적용  - findViewById 제거 하기

--- app/src/main/kotlin/com/androidhuman/example/simplegithub/ui/search/SearchAdapter.kt
+++ app/src/main/kotlin/com/androidhuman/example/simplegithub/ui/search/SearchAdapter.kt
@@ -1,20 +1,16 @@
 package com.androidhuman.example.simplegithub.ui.search
 
-import com.androidhuman.example.simplegithub.R
-import com.androidhuman.example.simplegithub.api.model.GithubRepo
-import com.androidhuman.example.simplegithub.ui.GlideApp
-
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.support.v7.widget.RecyclerView
 import android.text.TextUtils
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-
-import java.util.ArrayList
+import com.androidhuman.example.simplegithub.R
+import com.androidhuman.example.simplegithub.api.model.GithubRepo
+import com.androidhuman.example.simplegithub.ui.GlideApp
+import kotlinx.android.synthetic.main.item_repository.view.*
+import java.util.*
 
 class SearchAdapter : RecyclerView.Adapter<SearchAdapter.RepositoryHolder>() {
 
@@ -31,20 +27,23 @@ class SearchAdapter : RecyclerView.Adapter<SearchAdapter.RepositoryHolder>() {
     override fun onBindViewHolder(holder: RepositoryHolder, position: Int) {
         val repo = items[position]
 
-        GlideApp.with(holder.itemView.context)
-                .load(repo.owner.avatarUrl)
-                .placeholder(placeholder)
-                .into(holder.ivProfile)
+        with(holder.itemView) {
+            GlideApp.with(holder.itemView.context)
+                    .load(repo.owner.avatarUrl)
+                    .placeholder(placeholder)
+                    .into(ivItemRepositoryProfile)
 
-        holder.tvName.text = repo.fullName
-        holder.tvLanguage.text = if (TextUtils.isEmpty(repo.language))
-            holder.itemView.context.getText(R.string.no_language_specified)
-        else
-            repo.language
+            tvItemRepositoryName.text = repo.fullName
+            tvItemRepositoryLanguage.text =
+                    if (TextUtils.isEmpty(repo.language))
+                        holder.itemView.context.getText(R.string.no_language_specified)
+                    else
+                        repo.language
 
-        holder.itemView.setOnClickListener {
-            if (null != listener) {
-                listener!!.onItemClick(repo)
+            setOnClickListener {
+                if (null != listener) {
+                    listener!!.onItemClick(repo)
+                }
             }
         }
     }
@@ -65,25 +64,11 @@ class SearchAdapter : RecyclerView.Adapter<SearchAdapter.RepositoryHolder>() {
         this.items.clear()
     }
 
-    class RepositoryHolder(parent: ViewGroup) : RecyclerView.ViewHolder(LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_repository, parent, false)) {
-
-        var ivProfile: ImageView
-
-        var tvName: TextView
-
-        var tvLanguage: TextView
-
-        init {
-
-            ivProfile = itemView.findViewById(R.id.ivItemRepositoryProfile)
-            tvName = itemView.findViewById(R.id.tvItemRepositoryName)
-            tvLanguage = itemView.findViewById(R.id.tvItemRepositoryLanguage)
-        }
-    }
+    class RepositoryHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+            LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_repository, parent, false))
 
     interface ItemClickListener {
-
         fun onItemClick(repository: GithubRepo)
     }
 }

195b85c6b1af3fcc7bea9cdb8016d9bfc5d88231
Younghyun Kim <yhyun.kim@gmail.com>
2018-11-15 03:11:36 +0900

    optimize import

--- app/src/main/kotlin/com/androidhuman/example/simplegithub/extensions/RxJavaExtensions.kt
+++ app/src/main/kotlin/com/androidhuman/example/simplegithub/extensions/RxJavaExtensions.kt
@@ -1,10 +1,8 @@
 package com.androidhuman.example.simplegithub.extensions
 
-import android.arch.lifecycle.Lifecycle
-import android.arch.lifecycle.LifecycleObserver
+import com.androidhuman.example.simplegithub.rx.AutoClearedDisposable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
-import com.androidhuman.example.simplegithub.rx.AutoClearedDisposable
 
 operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
     this.add(disposable)
